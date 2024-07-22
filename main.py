2d5f54ce6c9318a4e6e2d9dde69d84027b8a419a
Hendrick Castro <hendrickcastro@outlook.com>
2024-07-22 15:32:31 +0200

    fine

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import ptvsd
+import debugpy
 import uvicorn
 from app_factory import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Habilitar ptvsd (Python Tools for Visual Studio Debugging)
-    ptvsd.enable_attach(address=('0.0.0.0', 5679), redirect_output=True)
-
-    # No detener la ejecución esperando el depurador para permitir recarga automática
-    # ptvsd.wait_for_attach()
+    # Habilitar debugpy (Python Tools for Visual Studio Debugging)
+    debugpy.listen(("0.0.0.0", 5679))
+    print("Esperando al depurador para adjuntar...")
+    # Puedes descomentar la siguiente línea si deseas que la ejecución se detenga hasta que el depurador se conecte
+    # debugpy.wait_for_client()
 
     # Pasar la aplicación como cadena de importación para soportar recarga automática
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
